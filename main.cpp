61431c852ee7f4748f71b5f6f060c7c5fe7b47c7
JonaKss <jona.correapc@gmail.com>
2020-04-02 13:44:14 -0300

    Fixed main windows closes itself

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-using namespace sf;
+int main(int argc, char *argv[]){
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-int main(){
-    sf::RenderWindow window(VideoMode(200, 200), "SFML works!");
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
 
-    
     return 0;
-};
+}
