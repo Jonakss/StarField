616c1ae426469d52c230d5bb8a5ca10662aa284d
JonaKss <jona.correapc@gmail.com>
2020-04-02 13:25:39 -0300

    Project started

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main(){
+    sf::RenderWindow window(VideoMode(200, 200), "SFML works!");
+
+    
+    return 0;
+};
